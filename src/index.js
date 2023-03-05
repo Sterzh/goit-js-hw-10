04b30f2e0242ab19f6a261468256430c17744191
Serhii Tomashevskyi <tom5423000@gmail.com>
2023-03-06 00:41:37 +0200

    save

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './css/styles.css';
+import API from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+
+input.addEventListener('focusout', onSearch);
+
+function onSearch() {
+  const countryName = input.value;
+
+  API.fetchCountries(countryName)
+    .then(() => {
+      console.log(countryName);
+    })
+    .catch(onFetchError);
+}
+
+function onFetchError(error) {
+  console.log('ошибка');
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,11 @@
-export default function fetchCountries() {}
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+function fetchCountries(countriesName) {
+  return fetch(
+    `${BASE_URL}/name/${countriesName}?fields=name.official,capital,population,flags,languages`
+  ).then(r => {
+    r.json();
+  });
+}
+
+export default { fetchCountries };
