80e2a39a05ea389e5ebbeb35c669b40b2a0845a5
Serhii Tomashevskyi <tom5423000@gmail.com>
2023-03-09 17:23:20 +0200

    save

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 import './css/styles.css';
 import API from './js/fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
-const input = document.querySelector('#search-box');
 
-input.addEventListener('focusout', onSearch);
+const input = document.querySelector('#search-box');
+const countriesList = document.querySelector('.country-list');
+
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch() {
-  const countryName = input.value;
+  const countryName = input.value.trim();
 
-  API.fetchCountries(countryName)
-    .then(() => {
-      console.log(countryName);
+  if (countryName.length > 0) {
+    API.fetchCountries(countryName)
+      .then(countries => {
+        renderCountries(countries);
+      })
+      .catch(error => {
+        onFetchError(error);
+      });
+  }
+}
+
+function renderCountries(countries) {
+  const markup = countries
+    .map(({ name, capital, population, flags, languages }) => {
+      return `<li><h2>${name}</h2><p>Capital: <span>${capital}</span></p><p>Population: <span>${population}</span></p><p>Languages: <span>${languages.name}</span></p></li>`;
     })
-    .catch(onFetchError);
+    .join('');
+  countriesList.innerHTML = markup;
+
+  console.dir(countries);
 }
 
 function onFetchError(error) {
-  console.log('ошибка');
+  console.log(error);
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,10 @@
-const BASE_URL = 'https://restcountries.com/v3.1';
+const BASE_URL = 'https://restcountries.com/v2';
 
 function fetchCountries(countriesName) {
   return fetch(
-    `${BASE_URL}/name/${countriesName}?fields=name.official,capital,population,flags,languages`
-  ).then(r => {
-    r.json();
+    `${BASE_URL}/name/${countriesName}?fields=name, name.official,capital,population,flags,languages`
+  ).then(response => {
+    return response.json();
   });
 }
 
